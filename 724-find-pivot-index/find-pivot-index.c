5fd5b01442ae1f1e1e33c02f68ca4965c9395f14
Santhiya1308 <m.santhiyamuthuramalingam7@gmail.com>
2026-02-26 21:07:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (95.53%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.c
@@ -0,0 +1,15 @@
+int pivotIndex(int* nums, int numsSize) {
+    int ts=0;
+    int ls=0;
+    for(int i=0;i<numsSize;i++){
+    ts+=nums[i];}
+    for(int i=0;i<numsSize;i++){
+        int rs=ts-ls-nums[i];
+        if(ls==rs)
+        {
+            return i;
+        }
+        ls+=nums[i];
+    }
+    return-1;
+}
